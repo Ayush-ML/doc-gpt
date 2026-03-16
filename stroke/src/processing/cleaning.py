87679838b665e2cf50af162895fec2dc100d7501
Ayush <ayushmanoj279@gmail.com>
2026-03-16 15:12:34 +0530

    Data Processing Completed

--- stroke/src/processing/cleaning.py
+++ stroke/src/processing/cleaning.py
@@ -5,15 +5,13 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 
+np.random.seed(42) # Set Random Seed for Reproducibility
+
 # Load The Dataset
 
-data = pd.read_csv(r'stroke\data\original-data.csv')
+data = pd.read_csv(r'stroke\data\raw_data\stroke-dataset.csv')
 data.columns = data.columns.str.strip()  # Strip whitespace from column names
 data = data.drop(columns='id', axis=1)  # Drop the 'id' column as it may harm the model
-
-# Correct The Data types of columns
-
-data['bmi'] = data['bmi'].astype(float)
 
 # Remove Very Rare Categories
 
@@ -35,12 +33,16 @@ missing_index = data[data['smoking_status'].str.contains('Unknown')]['smoking_st
 for index in missing_index:
     data.at[index, 'smoking_status'] = np.nan
 
+# Correct The Data types of columns
+
+data['bmi'] = data['bmi'].astype(float)
+
 # Split Dataset into X and y and further into train and test
 
 X = data.drop('stroke', axis=1)  # Features
 y = data['stroke']  # Target variable
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)  # Stratify to maintain class distribution in train and test sets
 
 # Turn Cleaned Data into Train and Test CSV Files
 
---
+++ stroke/src/processing/preprocessing.py
@@ -0,0 +1,58 @@
+# This is a Data Preprocessing Script for a Stroke Prediction Model
+# Imported Libraries Libraries
+
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import KBinsDiscretizer, OrdinalEncoder, OneHotEncoder, PolynomialFeatures
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+import joblib
+
+np.random.seed(42) # Set Random Seed for Reproducibility
+
+# Load The Cleaned Dataset
+
+X_train = pd.read_csv(r'stroke\data\processed_data\train\X_train.csv')
+y_train = pd.read_csv(r'stroke\data\processed_data\train\y_train.csv')
+
+X_test = pd.read_csv(r'stroke\data\processed_data\test\X_test.csv')
+y_test = pd.read_csv(r'stroke\data\processed_data\test\y_test.csv')
+
+# Create Preprocessing Pipelines for Numerical, Binary, and Categorical Features
+# Select Numerical, Binary , Categorical and Columns with Missing Values (likely only bmi and smoking_status)
+
+numeric_features = X_train.select_dtypes(include=['float64']).columns
+binary_cols = X_train.select_dtypes(include="object").columns
+binary_cols = [col for col in binary_cols if X_train[col].nunique() == 2]
+categorical_cols = X_train.select_dtypes(include="object").columns
+categorical_cols = [col for col in categorical_cols if X_train[col].nunique() > 2]
+
+# Create Pipelines for each type of feature
+
+bmi_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='mean'))
+])                                                   # Use 2 seperate Imputers with seperate strategies for the numerical BMI column and the categorical smoking_status column
+smoking_status_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='most_frequent'))
+])
+binary_transformer = Pipeline(steps=[
+    ('encoder', OrdinalEncoder(handle_unknown="use_encoded_value", unknown_value=-1))
+])
+categorical_transformer = Pipeline(steps=[
+    ('encoder', OneHotEncoder(handle_unknown='ignore'))
+])
+numerical_transformer = Pipeline(steps=[ # Use PolyNomialFeatures to create interaction and a discretionizer to split numerical columns into multiple features
+    ('poly', PolynomialFeatures(interaction_only=True, include_bias=False)),
+    ('discretizer', KBinsDiscretizer(n_bins=4, encode='onehot-dense', strategy='quantile')),
+])
+
+preprocessor = ColumnTransformer(transformers=[ # Create a ColumnTransformer to combine these pipelines together
+    ('bmi', bmi_transformer, ['bmi']),
+    ('smoking_status', smoking_status_transformer, ['smoking_status']),
+    ('numerical', numerical_transformer, numeric_features),
+    ('binary', binary_transformer, binary_cols),
+    ('categorical', categorical_transformer, categorical_cols),
+], remainder='passthrough')
+
+joblib.dump(preprocessor, r'stroke\src\saved\preprocessor.joblib') # Save the Preprocessor for later use in the model building and inference stages
